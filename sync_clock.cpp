f428b1aed66f03db830a4c7fb2fd541d95c3614f
KuroVine9 <kurovine9@gmail.com>
2022-05-15 06:58:48 +0900

    4비트 동기식 이진 상향 카운터

---
+++ sync_clock.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "FlipFlop.h"
+
+using std::cout;
+using std::endl;
+
+int main() {
+	TFF tff0 = TFF();
+	TFF tff1 = TFF();
+	TFF tff2 = TFF();
+	TFF tff3 = TFF();
+
+	bool bit0, bit1, bit2, bit3;
+	bool clock=true;
+
+	bit0 = tff0.LchOutput();
+	bit1 = tff1.LchOutput();
+	bit2 = tff2.LchOutput();
+	bit3 = tff3.LchOutput();
+
+	for (int i = 0; i < 20; i++) {
+		bit3 = tff3.TIO((bit0 & bit1 & bit2), clock);
+		bit2 = tff2.TIO((bit0 & bit1), clock);
+		bit1 = tff1.TIO(bit0, clock);
+		bit0 = tff0.TIO(1, clock);
+
+		tff3.ClockFall();
+		tff2.ClockFall();
+		tff1.ClockFall();
+		tff0.ClockFall();
+		
+		cout << "Loop " << i << endl;
+		cout << bit3 << bit2 << bit1 << bit0 << endl;
+		cout << "====" << endl;
+	}
+
+	return 0;
+}
